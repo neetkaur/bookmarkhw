b4b2065fb5db9ec196b23c47073fbc83e3b5c158
Neet <navneetkaur3@gmail.com>
2021-08-14 12:08:49 -0500

    bookmark completed

--- bookmark/models/bookmark.js
+++ bookmark/models/bookmark.js
@@ -3,6 +3,6 @@ const {Schema, model} = require('mongoose');
 const bm = new Schema({
   name: String,
   url: String
-},{timeStamps:true})
+},{timestamps:true})
 
 module.exports = model('Bookmark', bm)
